85b231f67ec0ea4eeb407eea5ef34df84cac1570
Jasand Pereza <jasandpereza@gmail.com>
2017-05-22 15:51:04 -0600

    force rebuild

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 
-// libs and stuff
+// libs
 if(typeof window != 'undefined') {
     require('./../../node_modules/waypoints/lib/jquery.waypoints.min.js');
     require('./../../node_modules/waypoints/lib/shortcuts/sticky.min.js');
